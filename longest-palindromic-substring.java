d25cbdabde86b9787a34d7c64ef09bc836d1de82
Md. A. Barik <48405620+mdabarik@users.noreply.github.com>
2023-06-16 23:28:08 +0600

    Update longest-palindromic-substring.java

--- longest-palindromic-substring.java
+++ longest-palindromic-substring.java
@@ -38,3 +38,33 @@ class Main {
         return dfs(s, 0, n - 1);
     }
 }
+
+
+import java.util.*;
+
+class Main {
+    public static int findLpsLength(String s){ 
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+        for (int start = n - 2; start >= 0; start--) {
+            for (int end = start + 1; end < n; end++) {
+                int max = 0;
+                if (s.charAt(start) == s.charAt(end)) {
+                    if (end - start + 1 == 2) {
+                        max = 2;
+                    } else if (end - start + 1 == 2 + dp[start + 1][end - 1]) {
+                        max = end - start + 1; 
+                    }
+                }
+                int left = dp[start + 1][end];
+                int right = dp[start][end - 1];
+                dp[start][end] = Math.max(max, Math.max(left, right));
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
+
