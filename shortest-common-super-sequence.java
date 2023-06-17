6dba1e5f07f7b80774e77f21d9a70d394af04544
Md. A. Barik <48405620+mdabarik@users.noreply.github.com>
2023-06-17 11:52:12 +0600

    Update shortest-common-super-sequence.java

--- shortest-common-super-sequence.java
+++ shortest-common-super-sequence.java
@@ -34,3 +34,25 @@ public class Main {
         return dfs(str1, str2, 0, 0, dp);
     }
 };
+
+
+public class Main {
+    public static int shortestCommonSupersequence(String str1, String str2) {
+        int n1 = str1.length(), n2 = str2.length();
+        int[][] dp = new int[n1 + 1][n2 + 1];
+        for (int i = 0; i < dp.length; i++)
+            dp[i][0] = i;
+        for (int i = 0; i < dp[0].length; i++) 
+            dp[0][i] = i;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+}
