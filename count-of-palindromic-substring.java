53e43849a9ea18942145a6cfd825246328157705
Md. A. Barik <48405620+mdabarik@users.noreply.github.com>
2023-06-19 18:34:27 +0600

    Update count-of-palindromic-substring.java

--- count-of-palindromic-substring.java
+++ count-of-palindromic-substring.java
@@ -56,3 +56,33 @@ public class Main{
     }
 }
 // TC: O(n^2), SC: O(n^2)
+
+
+import java.util.*;
+
+public class Main{
+    public static Integer countPalindromicSubstring (String str1) {
+        int n = str1.length();
+        boolean[][] dp = new boolean[n + 1][n + 1];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+        int count = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (str1.charAt(i) == str1.charAt(j)) {
+                    if (i+1 >= j)
+                        dp[i][j] = true;
+                    else
+                        dp[i][j] = dp[i + 1][j - 1];
+                }
+                
+                if (dp[i][j] == true) 
+                count++;
+            }
+        }
+        return str1.length() + count;
+    }
+}
+
+// TC: O(n^2), SC: O(n^2)
